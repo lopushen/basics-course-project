900fe97026fe5c559157d605e0516c686ff9077e
Ievgenii Lopushen <lopushen@gmail.com>
2019-07-10 12:11:42 +0300

    basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import {RecipesListComponent} from './recipes/recipes-list/recipes-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component(
+  {
+    selector: 'app-recipes-list',
+    templateUrl: 'recipes-list.component.html'
+  }
+)
+export class RecipesListComponent {}
