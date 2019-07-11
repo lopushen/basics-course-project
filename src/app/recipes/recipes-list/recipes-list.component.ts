1380442b9555b42e48c9fc764e223b97d0f73c15
Ievgenii Lopushen <lopushen@gmail.com>
2019-07-11 23:53:44 +0300

    added ingredients

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component(
   {
@@ -6,4 +7,12 @@ import {Component} from '@angular/core';
     templateUrl: 'recipes-list.component.html'
   }
 )
-export class RecipesListComponent {}
+export class RecipesListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Pudding', 'This is a pudding recipe',
+      'https://thepioneerwoman.com/wp-content/uploads/2016/04/' +
+      'how-to-make-chocolate-pudding-00a.jpg?resize=780%2C521?w=1024'),
+    new Recipe('Omlette', 'The Omlette recipe', 'https://x9wsr1khhgk5pxnq1f1r8kye-wpengine.netdna-ssl.com/' +
+      'wp-content/uploads/basic-french-omelet-930x550.jpg')
+  ];
+}
